2050fa3bd6732e43c340661ec8d6377ffc6b7acd
Luiz Guilherme <luizgmelo64@gmail.com>
2025-05-11 20:30:03 -0300

    feat: create getters and setters pet entity

--- src/main/java/com/luizgmelo/sistema_cadastro_pet/models/Pet.java
+++ src/main/java/com/luizgmelo/sistema_cadastro_pet/models/Pet.java
@@ -2,11 +2,19 @@ package com.luizgmelo.sistema_cadastro_pet.models;
 
 import com.luizgmelo.sistema_cadastro_pet.enums.PetType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_pet")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
